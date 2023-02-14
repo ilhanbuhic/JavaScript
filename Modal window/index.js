9dabb3282eb985563a0ff64f2420c6a8d71231d5
ilhanbuhic <ilhanbuhic@hotmail.com>
2023-02-14 20:54:35 +0100

    JavaScript

---
+++ Modal window/index.js
@@ -0,0 +1,34 @@
+'use strict'
+
+const modal = document.querySelector('.modal')
+const overlay = document.querySelector('.overlay')
+const btnCloseModal = document.querySelector('.close-modal')
+const btnsOpenModal = document.querySelectorAll('.show-modal')
+
+function openModal() {
+  modal.classList.remove('hidden')
+  overlay.classList.remove('hidden')
+}
+
+function closeModal() {
+  modal.classList.add('hidden')
+  overlay.classList.add('hidden')
+}
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', openModal)
+  // modal.style.display = 'block'
+  // overlay.style.display = 'block'
+}
+
+btnCloseModal.addEventListener('click', closeModal)
+// modal.style.display = 'none'
+// overlay.style.display = 'none'
+
+overlay.addEventListener('click', closeModal)
+
+document.addEventListener('keyup', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal()
+  }
+})
