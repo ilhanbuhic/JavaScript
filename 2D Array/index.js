263d67e7dd49f818fe2eec5983385afb4e0ca8f8
ilhanbuhic <ilhanbuhic@hotmail.com>
2023-02-16 00:14:04 +0100

    JavaScript

---
+++ 2D Array/index.js
@@ -0,0 +1,19 @@
+// Making 2D array
+
+let array = [
+    [1,2,3,4,5],
+    [6,7,8,9,10],
+    [11,12,13,14, 15],
+    [16,17,18,19,20]
+]
+matrica = ""
+
+
+for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array[i].length; j++) {
+    }
+    console.log(array[i])
+}
+
+
+
