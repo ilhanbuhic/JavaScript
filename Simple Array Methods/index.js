c2074c6fca0d16fa44fa3d4b3650b6cb2d577163
ilhanbuhic <ilhanbuhic@hotmail.com>
2023-05-22 22:33:41 +0200

    JavaScript

--- Simple Array Methods/index.js
+++ Simple Array Methods/index.js
@@ -37,3 +37,38 @@ console.log([...arr])
 console.log(arr.slice())
 // This is a matter of personal preference
 // The only time we need to use the .slice() method is when we want to chain multiple methods together, calling one after the other
+
+console.log("----- SPLICE -----")
+// .splice() method:
+// .splice() works in almost the same way as .slice()
+// The fundamental difference is that it does change the original array, it mutates that array
+console.log(arr.splice(2)) // output: [c, d, e]
+console.log(arr) // output: [a, b]
+// The extracted elements are gone from the original array, .splice() deleted them
+// Splice does mutate the original array
+// It takes part of the array, returns it, then the original array itself loses this part that was extracted
+// In practice, most of the time, the value that the .splice() method returns doesn't even interest us
+// We are usually interested in is to just delete one or more elements from an array using .splice()
+// Common use case is to remove the last elements of an array
+console.log(arr.splice(-1))
+
+console.log(arr.splice(1, 2))
+// The first parameter works the same as in the .slice() method, but the second one is the number of elements that we want to delete
+
+console.log("----- REVERSE -----")
+// This method reverses the array
+arr = ["a", "b", "c", "d", "e"]
+const arr2 = ["j", "i", "h", "g", "f"]
+console.log(arr2.reverse())
+// Reverse method doesn't mutate the original array
+
+console.log("----- CONCAT -----")
+// This method is used to concatenate two arrays
+const letters = arr.concat(arr2)
+console.log(letters)
+// The first array will be the one on which the method is called and the second one is the one that we pass into the .concat() method
+// We can also concat arrays using the spread operator
+
+console.log("---- JOIN ----")
+console.log(letters.join("-"))
+// The result is a striing with the separator that we specified
