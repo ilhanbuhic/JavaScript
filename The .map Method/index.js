ac31b3f35217d5063b4dc5a2706455b78b75dfa6
ilhanbuhic <ilhanbuhic@hotmail.com>
2023-05-30 22:08:55 +0200

    JavaScript

---
+++ The .map Method/index.js
@@ -0,0 +1,27 @@
+"use strict"
+
+// .map() method:
+// The .map() method is another way that we can use to loop over an array
+// Unlike .forEach, the .map() method will give us a brand new array
+// This new array will contain in each position the results of applying a callback function to the original array elements
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300]
+// Supposing that these movements are in Euros, we will try to convert them to USD
+// Supposing that 1 Euro is 1.1 USD
+
+const eurToUsd = 1.1
+// We want to multiply each elements of the movements array by 1.1
+
+const movementsUSD = movements.map(function (mov) {
+  // Here in the callback function, we need to return the value
+  //that we want the new array to have in the current position
+  return mov * eurToUsd
+})
+
+console.log(movements)
+console.log(movementsUSD)
+
+// Same example using the ARROW function:
+console.log(`----- ARROW FUNCTION -----`)
+const movementsUSDArrowFunc = movements.map((mov) => mov * eurToUsd)
+console.log(movementsUSDArrowFunc)
