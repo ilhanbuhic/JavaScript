3f9b619bd3011867bb0ca51d20840670c7a6ac4f
ilhanbuhic <ilhanbuhic@hotmail.com>
2023-06-13 23:58:13 +0200

    JavaScript

--- Chaining Methods - .map, .filter, .reduce/index.js
+++ Chaining Methods - .map, .filter, .reduce/index.js
@@ -19,3 +19,31 @@ console.log(totalDepositsUSD.toFixed())
 // We can imagine all of this as a 'PIPELINE' that processes our data
 // We put the data at the begining, which goes through all the steps (line: 10/13)
 // In the end, our input data comes out processed on the other side of the 'pipeline'
+
+// When we chain all the methods together, it can be hard to debug if one of the results is not what we expect
+// Sometimes we can't know from which step of this 'pipeline' the bug would come from
+// To solve this, it would be good to check out the array in each of these different steps
+
+// We can check what's causing the bug in this way:
+
+// const eurToUSD = 1.1
+// const totalDepositsUSD = movements
+//   .filter((mov) => mov > 0)
+//   .map((mov, _, arr) => {
+//   // this array has to be the result of the previous operation, of this .filter()
+//     console.log(arr)
+//     return mov * eurToUSD
+//   })
+//   .reduce((acc, mov) => acc + mov, 0)
+// console.log(totalDepositsUSD.toFixed())
+
+/* Remarks about chaining:
+1. We shouldn't overuse chaining, we should try to optimize it, 
+because chaining tons of methods can cause performance issues if we have huge arrays
+If we have a huge chain of methods, chained one after another, 
+we should try to compress all the functionality that they do into as little methods as possible
+Sometimes we can create way more .map methods than we need, where we could do it in just 1 .map call
+
+2. It is a bad practice in JavaScript to chain methods that mutate the original array
+We shouldn't chain methods like .splice() and or the .reverse() method
+*/
