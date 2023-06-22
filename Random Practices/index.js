f41dd4d0424e4b447d014451af2d14e191c388b6
ilhanbuhic <ilhanbuhic@hotmail.com>
2023-06-22 17:24:08 +0200

    JavaScript

--- Random Practices/index.js
+++ Random Practices/index.js
@@ -111,3 +111,21 @@
 // console.log(newWords)
 // 5. ----------------------------------------------------------------
 
+// 6. ----------------------------------------------------------------
+// Make the first and last name starting with the uppercase letter
+// const fullName = ["ilhan", "buhic"]
+
+// Solution #1:
+// fullName.forEach(function (curr) {
+//   console.log(
+//     curr[0].replace(curr[0], curr[0].toUpperCase()) + curr.slice(1, curr.length)
+//   )
+// })
+
+// Solution #2:
+// const changeName = function (arr) {
+//   return arr.map((name) => name[0].toUpperCase() + name.slice(1))
+// }
+// console.log(changeName(fullName))
+// 6. ----------------------------------------------------------------
+
