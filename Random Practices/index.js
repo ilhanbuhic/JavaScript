4c3258cf27799b17a1a95c1052ce9fdd34e35c6d
ilhanbuhic <ilhanbuhic@hotmail.com>
2023-06-22 17:21:43 +0200

    JavaScript

--- Random Practices/index.js
+++ Random Practices/index.js
@@ -88,3 +88,26 @@
 // }
 // console.log(arr)
 // 4. ----------------------------------------------------------------
+
+// 5. ----------------------------------------------------------------
+// Seperate the words that have the letter 'a' as the second letter of the word
+// let words = [
+//   "Banana",
+//   "Animal",
+//   "Amazing",
+//   "Attack",
+//   "Annual",
+//   "Against",
+//   "Car",
+//   'Map'
+// ]
+// let newWords = []
+
+// for (let i = 0; i < words.length; i++) {
+//   if (words[i][1] === "a") {
+//     newWords.push(words[i])
+//   }
+// }
+// console.log(newWords)
+// 5. ----------------------------------------------------------------
+
