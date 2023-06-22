a7cc9eb5ea5bba252cbef71ff326c3645f71fc88
ilhanbuhic <ilhanbuhic@hotmail.com>
2023-06-22 17:26:23 +0200

    JavaScript

--- Random Practices/index.js
+++ Random Practices/index.js
@@ -129,3 +129,22 @@
 // console.log(changeName(fullName))
 // 6. ----------------------------------------------------------------
 
+// 7. ----------------------------------------------------------------
+// Separating only first and last names
+// let fullArr = ["David", "John", "Roger", "Buhic", 2, "Sterenborg", false]
+
+// let names = ["David", "John", "Roger"]
+// let lastNames = ["Buhic", "Sterenborg"]
+
+// let namesOnly = []
+// let lastNamesOnly = []
+
+// for (let i = 0; i < fullArr.length; i++) {
+//   if (names.includes(fullArr[i])) {
+//     namesOnly.push(fullArr[i])
+//   } else if (lastNames.includes(fullArr[i])) {
+//     lastNamesOnly.push(fullArr[i])
+//   }
+// }
+// 7. ----------------------------------------------------------------
+
