2f93d0a74b0fa54675938f1ed31ade9321223837
ilhanbuhic <ilhanbuhic@hotmail.com>
2023-07-03 23:37:29 +0200

    JavaScript

---
+++ .findIndex Method/index.js
@@ -0,0 +1,6 @@
+"use strict"
+
+// .findIndex() method works almost the same way as .find(),
+// but .findIndex() returns an index of found element and not the element itself
+// To delete an element from an array, we use the .splice() method, but for the .splice() method
+// we need the index at which we want to delete
