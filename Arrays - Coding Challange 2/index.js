8e5aef4c12b003ec15811777a71798745afde89f
ilhanbuhic <ilhanbuhic@hotmail.com>
2023-07-17 23:49:51 +0200

    JavaScript

--- Arrays - Coding Challange 2/index.js
+++ Arrays - Coding Challange 2/index.js
@@ -114,3 +114,56 @@ HINT: Some dogs have multiple owners, so you first need to find Sarah in the own
 // // const dogsCopy = [...dogs].sort((a, b) => a.recommendedPortion - b.recommendedPortion) // This also works
 // // We can also use .slice() method without any arguments to copy the array, instead of [...dogs]
 // console.log(dogsCopy)
+
+// The second way
+
+const dogs = [
+    { weight: 22, curFood: 278, owners: ["Alice", "Bob"] },
+    { weight: 8, curFood: 200, owners: ["Matilda"] },
+    { weight: 13, curFood: 275, owners: ["Sarah", "John"] },
+    { weight: 32, curFood: 340, owners: ["Michael"] },
+  ]
+  
+  // 1.
+  dogs.forEach((dog) => (dog.recFood = Math.trunc(dog.weight ** 0.75 * 28)))
+  
+  // 2.
+  const dogSarah = dogs.find((dog) => dog.owners.includes("Sarah"))
+  console.log(
+    `Sarah's dog is eating ${
+      dogSarah.curFood > dogSarah.recFood ? "too much" : "too little"
+    }`
+  )
+  
+  // 3.
+  const ownersEatTooMuch = dogs
+    .filter((dog) => dog.curFood > dog.recFood)
+    .flatMap((dog) => dog.owners)
+  const ownersEatTooLittle = dogs
+    .filter((dog) => dog.curFood < dog.recFood)
+    .flatMap((dog) => dog.owners)
+  console.log(ownersEatTooMuch)
+  console.log(ownersEatTooLittle)
+  
+  // 4.
+  console.log(`${ownersEatTooMuch.join(" and ")}'s dogs are eating too much`)
+  console.log(`${ownersEatTooLittle.join(" and ")}'s dogs are eating too little`)
+  
+  // 5.
+  console.log(dogs.some((dog) => dog.curFood === dog.recFood))
+  
+  // 6.
+  /* We are setting this function as an arrow function,
+  so we can later use it as a callback function
+  */
+  const checkEatingOkay = (dog) =>
+    dog.curFood > dog.recFood * 0.9 && dog.curFood < dog.recFood * 1.1
+  console.log(dogs.some(checkEatingOkay))
+  
+  // 7.
+  console.log(dogs.filter(checkEatingOkay))
+  
+  // 8.
+  const dogsSorted = [...dogs].sort((a, b) => a.recFood - b.recFood)
+  console.log(dogsSorted)
+  
