3adbd3bd1734c7b503fc88570217892dae60b0e4
ilhanbuhic <ilhanbuhic@hotmail.com>
2023-08-04 23:42:12 +0200

    JavaScript

--- Creating Dates/index.js
+++ Creating Dates/index.js
@@ -62,3 +62,35 @@ const account2 = {
 // // Converting days to miliseconds
 // console.log(new Date(3 * 24 * 60 * 60 * 1000))
 // // The number that we calculated (line: 61) is a timestamp (number 259200000). It's a timestamp of the day number 3
+
+// Dates are another type of object, which means that they have their own methods
+// We can use these methods to get or to set components of a date
+// Working with dates:
+const future = new Date(2025, 10, 19, 15, 23)
+console.log(future)
+console.log(future.getFullYear()) // This method is used for showing the year
+console.log(future.getMonth()) // This method is used for showing the month
+console.log(future.getDate()) // This method is used for showing the day
+console.log(future.getDay()) // This method is used for showing the day of the week
+// When using .getDay() method, days are zero-based
+console.log(future.getHours())
+console.log(future.getMinutes())
+console.log(future.getSeconds())
+
+// We can also use ISO string for nicely formatted dates
+console.log(future.toISOString())
+// We can use this when we want to convert a particular date object into a string, so we can store it somewhere
+
+// We can also get a timestamp for the date
+// Timestamp is a miliseconds, which have passed since January 1, 1970
+console.log(future.getTime()) // result: 1763562180000
+// Now we can take this timestamp number and reverse (line: 84)
+// We can create a new date only based on these miliseconds
+console.log(new Date(future.getTime())) // It will give us exactly that same number: 2025-11-19T14:23:00.000Z
+
+// Timestamps are so important, that there is a method that we can use to get the timestamp for right now
+console.log(Date.now()) // This method is giving us the current timestamp
+
+// There are also set version of all of these methods
+future.setFullYear(2040) // In this case, only the day of the month will change
+// This also perform autocorrection just like when we create a new date
