dfc7c3f79e236521e0c1e702e576bd2a27c036ff
Ilhan Buhic <ilhanbuhic@hotmail.com>
2023-10-09 22:09:45 +0200

    practice

--- Random Practices/index.js
+++ Random Practices/index.js
@@ -324,3 +324,21 @@
 
 // 13. ----------------------------------------------------------------
 
+// 14. ----------------------------------------------------------------
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+// You can return the answer in any order.
+
+var twoSum = function(nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = 0; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                return [i, j]
+            }
+        }
+    }
+};
+
+// console.log(twoSum([1, 2, 3, 4], 3));
+// 14. ----------------------------------------------------------------
+
