ef2b0d615810a7108d6f554ce6daddbfe9d6074e
Ilhan Buhic <ilhanbuhic@hotmail.com>
2023-12-25 19:38:29 +0100

    practice

--- Random Practices/index.js
+++ Random Practices/index.js
@@ -476,6 +476,8 @@
 // console.log(combinations.join('\n'))
 // 15. ----------------------------------------------------------------
 
+// 16. ----------------------------------------------------------------
+
 // Find on which index is the target
 
 // Solution #1:
@@ -494,24 +496,55 @@
 // console.log(findTarget(nums, target));
 
 // Solution #2:
-let nums = [-1, 0, 3, 5, 9, 12]
-let target = 9
+// let nums = [-1, 0, 3, 5, 9, 12]
+// let target = 9
 
-function binarySearch(nums, target) {
-  let left = 0
-  let right = nums.length - 1
+// function binarySearch(nums, target) {
+//   let left = 0
+//   let right = nums.length - 1
 
-  while (left <= right) {
-    let mid = Math.floor((right+left) /2)
-    if (nums[mid] === target) return mid
+//   while (left <= right) {
+//     let mid = Math.floor((right+left) /2)
+//     if (nums[mid] === target) return mid
 
-    if (nums[mid] < target) {
-      left = mid + 1
-    } else {
-      right = mid - 1
-    }
-  }
-  return -1
+//     if (nums[mid] < target) {
+//       left = mid + 1
+//     } else {
+//       right = mid - 1
+//     }
+//   }
+//   return -1
+// }
+
+// console.log(binarySearch(nums, target))
+// 16. ----------------------------------------------------------------
+
+// 17. ----------------------------------------------------------------
+// Make a program that will group people by their names
+/* Output: {
+    Kyle: [{name: 'Kyle', age: 28}],
+    Sally: [
+        {name: 'Sally', age: 45},
+        {name: 'Sally', age: 28}
+    ],
+    John: [{name: 'John', age: 34}]
 }
+*/
 
-console.log(binarySearch(nums, target))
+// const people = [
+//   { name: 'Kyle', age: 28 },
+//   { name: 'Sally', age: 45 },
+//   { name: 'John', age: 34 },
+//   { name: 'Sally', age: 28 },
+// ]
+
+// const arrNoDuplicates = people.reduce((newObj, person) => {
+//   if (newObj[person.name] == null) {
+//     newObj[person.name] = []
+//   }
+//   newObj[person.name].push(person)
+//   return newObj
+// }, {})
+// console.log(arrNoDuplicates)
+
+// 17. ----------------------------------------------------------------
