2d45089058579798123ab87263c5ab00d443e5ef
Ilhan Buhic <ilhanbuhic@hotmail.com>
2024-01-11 17:49:41 +0100

    setTimeout and setInterval and how does it works

--- Timers - setTimeout and setInterval/index.js
+++ Timers - setTimeout and setInterval/index.js
@@ -20,3 +20,22 @@ setTimeout(
 // When the execution of our code reaches this point, it will call the setTimeout() function,
 // it will then register the callback function to be called later. Then the code execution continues
 console.log('Waiting...')
+
+// Using a setTimeout() with arguments
+setTimeout(
+  (ing1, ing2) => console.log(`Here is your pizza with ${ing1}, ${ing2}🍕`),
+  2000,
+  'olives',
+  'spinach'
+)
+
+// We can also do this:
+const ingredients = ['cheese', 'hambuger sauce']
+const burgerTimer = setTimeout(
+  (ing1, ing2) => console.log(`Hamburger with ${ingredients}`),
+  2500,
+  ...ingredients
+)
+
+// We can also cancel the timer, until the delay has actually passed
+if (ingredients.includes('hambuger sauce')) clearTimeout(burgerTimer)
