f9a1810f813d2e586d49acdf807c74b08d1c8ba1
Ilhan Buhic <ilhanbuhic@hotmail.com>
2024-01-24 23:31:59 +0100

    write comment for Constructor Functions

---
+++ Constructor Functions and the new Operator/index.js
@@ -0,0 +1,14 @@
+'use strict'
+
+/* 
+We can use constructor functions to build an object using a function. Constructor function is a completely normal function
+The only difference between a regular function and a function that we call constructior function, is that we call
+a constructor function with the NEW operator
+*/
+
+/*
+In OOP there is a convention that constructor function always start with a capital letter
+Both function expression and function declaration will work, but arrow function will not as a function constructor
+It's because it doesn't have its own .this keyword
+*/
+``
