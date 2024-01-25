5c4c1b8a2acbc339bf8e7dcf6a5b94e0396e380e
Ilhan Buhic <ilhanbuhic@hotmail.com>
2024-01-25 23:25:25 +0100

    add example

--- Constructor Functions and the new Operator/index.js
+++ Constructor Functions and the new Operator/index.js
@@ -11,4 +11,32 @@ In OOP there is a convention that constructor function always start with a capit
 Both function expression and function declaration will work, but arrow function will not as a function constructor
 It's because it doesn't have its own .this keyword
 */
-``
+
+// This function is going to produce an object
+const Person = function (firstName, birthytYear) {
+  // Taking the firstName parameter, then create a property on the .this keyword
+  // with the same name and set it to that value
+
+  /* These properties are the INSTANCE properties
+        This is because the firstName and birthYear
+        will be available on all the instances that
+        are created through this constructor function
+    */
+  this.firstName = firstName
+  this.birthYear = birthytYear
+
+  /* We can also add methods to our objects
+        Never do this
+        We should never create a method inside of a constructor
+        It's because if we make too many person objects,
+        each of these objects would carry around this function
+        Instead of this, we will use prototypes of prototype inheritance
+    */
+
+  // this.calcAge = function() {
+  //     console.log(2023 - this.birthYear)
+  // }
+}
+
+const ilhan = new Person('Ilhan', 1998)
+console.log(ilhan)
